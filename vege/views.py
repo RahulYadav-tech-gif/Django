f34542ddb2abfee66eaa4d5fca655ad194df7dfc
RahulYadav-tech-gif <104778554+RahulYadav-tech-gif@users.noreply.github.com>
2024-08-07 16:13:15 +0530

    views.py

--- vege/views.py
+++ vege/views.py
@@ -5,6 +5,8 @@ from django.contrib.auth.models import User
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
+from django.core.paginator import Paginator
+from django.db.models import Q, Sum
 
 
 
@@ -116,3 +118,35 @@ def register(request):
 
     return render(request, 'register.html')
 
+def get_students(request):
+    queryset = Student.objects.all()
+
+    if request.GET.get('search'):
+        search = request.GET.get('search')
+        queryset = queryset.filter(
+            Q(student_name__icontains = search)|
+            Q(department__department__icontains = search)|
+            Q(student_id__student_id__icontains = search)|
+            Q(student_email__icontains = search)|
+            Q(student_age__icontains = search)|
+            Q(student_address__icontains = search)
+        )
+    paginator = Paginator(queryset, 25)  # Show 25 contacts per page.
+    page_number = request.GET.get("page",1)
+    page_obj = paginator.get_page(page_number)
+
+    return render(request, 'students.html', {'queryset':page_obj})
+
+def see_marks(request, student_id):
+    queryset = SubjectMarks.objects.filter(student__student_id__student_id = student_id)
+    total_marks = queryset.aggregate(total_marks = Sum('marks'))
+    current_rank = -1
+    ranks = Student.objects.annotate(marks = Sum('studentmarks__marks')).order_by('-marks','-student_age')
+    i=1
+    for rank in ranks:
+        if student_id == rank.student_id.student_id:
+            current_rank = i
+            break
+        i = i+1
+            
+    return render(request,'see_marks.html', {'queryset':queryset, 'total_marks':total_marks, 'current_rank':current_rank})
